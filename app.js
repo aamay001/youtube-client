9855bebf707af5e5c0ef20e62b0544647446583d
Andy Amaya <aamay001@gmail.com>
2017-06-23 13:54:13 -0700

    adding search bar Updating author information.

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
+var YTC_SEARCH_SUBMIT_BUTTON = '.js-ytc-search-button';
+
 $(onReady);
 
 function onReady()
 {
+    bindUserInput();
+}
+
+function bindUserInput()
+{
+    $(YTC_SEARCH_SUBMIT_BUTTON).on('click', onSearchSubmitClick);
+}
+
+function onSearchSubmitClick(event)
+{
+    alert('Buton was clicked.');
 }
