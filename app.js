69515129684fa0dd69b6d1f19ef91db3c44f808e
Andy Amaya <aamay001@gmail.com>
2017-06-24 10:17:16 -0700

    search bar with stub functions

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 var YTC_SEARCH_SUBMIT_BUTTON = '.js-ytc-search-button';
+var YTC_API_KEY = 'AIzaSyDQMrY_PWD1wjWMpZotRPqdNbmgX-J_jlg';
+var TYC_YOUTUBE_SEARCH_ENDPOINT = 'https://www.googleapis.com/youtube/v3/search';
+var YTC_USER_INPUT = '#ytc-search-input';
 
 $(onReady);
 
@@ -9,10 +12,40 @@ function onReady()
 
 function bindUserInput()
 {
+    $(YTC_USER_INPUT).keydown(onUserInputKeyDown);
     $(YTC_SEARCH_SUBMIT_BUTTON).on('click', onSearchSubmitClick);
 }
 
-function onSearchSubmitClick(event)
+function onUserInputKeyDown(event)
 {
-    alert('Buton was clicked.');
+    if ( event.which == 13 )
+        onSearchSubmitClick();
+}
+
+function onSearchSubmitClick()
+{
+    var userInput = $(YTC_USER_INPUT).val();
+    searchYouTube(userInput);
+}
+
+function searchYouTube(searchText)
+{
+    var params = {
+                    part : 'snippet',
+                    resultsPerPage: 10,
+                    q : searchText,
+                    type : 'video',
+                    key : YTC_API_KEY
+                 };
+
+    $.getJSON(TYC_YOUTUBE_SEARCH_ENDPOINT, params, processSearchResults);
+}
+
+function processSearchResults(results)
+{
+    $.each( results, 
+        function(key, value)
+        { 
+            console.log(key + " : " + value);
+        });
 }
